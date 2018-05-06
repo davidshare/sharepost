e817f9ce220636e397a5f7ca848490c2bc22bb6f
David Essien <davidessienshare@gmail.com>
2018-05-06 23:43:14 +0100

    change config details to match database

--- app/config/config.php
+++ app/config/config.php
@@ -1,17 +1,17 @@
 <?php 
 
 	define('DB_HOST', 'localhost');
-	define('DB_USER', '_YOUR_USER_');
-	define('DB_PASS', '_YOUR_PASS_');
-	define('DB_NAME', '_YOUR_DB_NAME_');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'gemshare,php1989@');
+	define('DB_NAME', 'shareposts');
 	//App Root
 	define('APPROOT', dirname(dirname(__FILE__)));
 
 	//URL Root
-	define('URLROOT', '_ROOT_URL_');
+	define('URLROOT', 'http://localhost/sharepost/');
 
 	//site name
-	define('SITENAME', 'PHRAMEWORK');
+	define('SITENAME', 'SHAREPOST');
 
 
 ?>
