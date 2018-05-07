12bf757233b98f0ccfce20ce827ec0053a11c277
David Essien <davidessienshare@gmail.com>
2018-05-07 08:46:29 +0100

    Create login function and edit error fields

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -20,6 +20,7 @@
 					'email' => '',
 					'password' => '',
 					'confirm_password'=>'',
+					'name_err' => '',
 					'email_error'=>'',
 					'password_err'=>'',
 					'confirm_password_err'=>''
@@ -27,6 +28,25 @@
 
 				//Load view
 				$this->view('users/register', $data);
+			}
+		}
+
+		public function login(){
+			//Check for POST
+			if($_SERVER['REQUEST_METHOD'] == 'POST'){
+				//process form
+
+			}else{
+				//init data
+				$data = [
+					'email' => '',
+					'password' => '',
+					'email_error'=>'',
+					'password_err'=>''
+				];
+
+				//Load view
+				$this->view('users/login', $data);
 			}
 		}
 	}
