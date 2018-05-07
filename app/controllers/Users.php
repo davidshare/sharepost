ba47b0f76690a67b017c653c414726c72d12dc42
David Essien <davidessienshare@gmail.com>
2018-05-07 11:53:02 +0100

    add form validation for user registration

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -13,6 +13,56 @@
 			if($_SERVER['REQUEST_METHOD'] == 'POST'){
 				//process form
 
+				//Sanitize POST data
+				$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+				//Init data
+				$data = [
+					'name' => trim($_POST['name']),
+					'email' => trim($_POST['email']),
+					'password' => trim($_POST['password']),
+					'confirm_password' => trim($_POST['confirm_password']),
+					'name_err' => '',
+					'email_error'=>'',
+					'password_err'=>'',
+					'confirm_password_err'=>''
+
+				];
+
+				//Validate email
+				if(empty($data['email'])){
+					$data['email_err'] = 'Please enter email';
+				}
+
+				//Validate email
+				if(empty($data['name'])){
+					$data['name_err'] = 'Please enter name';
+				}
+
+				//Validate email
+				if(empty($data['password'])){
+					$data['password_err'] = 'Please enter password';
+				}elseif(strlen($data['password'])<8){
+					$data['password_err'] = 'your password must be more than 8 characters';
+				}
+
+				//Validate email
+				if(empty($data['confirm_password'])){
+					$data['confirm_password_err'] = 'Please enter Confirmation password';
+				}else{
+					if($data['password'] !== $data['confirm_password']){
+						$data['confirm_password_err'] = 'Passwords do not match';
+					}
+				}
+
+				//Make sure errors are empty
+				if(empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])){
+					//Validated
+					die('SUCCESS');
+				}else{
+					$this->view('users/register', $data);
+				}
+
 			}else{
 				//init data
 				$data = [
