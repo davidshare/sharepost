324edadffb20ce600caa0871a925eafe033fce5b
David Essien <davidessienshare@gmail.com>
2018-05-07 13:10:06 +0100

    add findUserByEmail function

--- app/models/User.php
+++ app/models/User.php
@@ -5,5 +5,20 @@
         public function __construct(){
             $this->db = new Database();
         }
+
+        //Find user by email
+        public function findUserByEmail($email){
+            $this->db->query('Select * From users WHERE email = :email');
+            $this->db->bind(':email', $email);
+
+            $row = $this->db->single();
+
+            //Check row
+            if($this->db-rowCount()>0){
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 ?>
