c9a93de998456143a5825e55560960e59dc89ea0
David Essien <davidessienshare@gmail.com>
2018-05-07 02:22:27 +0100

    add register function to Users class

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -2,10 +2,32 @@
 	/**
 	*  
 	*/
-	class Users{
+	class Users extends Controller{
 		
-		function __construct()
-		{
-			
+		function __construct(){
+				
+		}
+
+		public function register(){
+			//Check for POST
+			if($_SERVER['REQUEST_METHOD'] == 'POST'){
+				//process form
+
+			}else{
+				//init data
+				$data = [
+					'name' => '',
+					'email' => '',
+					'password' => '',
+					'confirm_password'=>'',
+					'email_error'=>'',
+					'password_err'=>'',
+					'confirm_password_err'=>''
+				];
+
+				//Load view
+				$this->view('users/register', $data);
+			}
+		}
 	}
 ?>
