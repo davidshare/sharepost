46f716b39dd9a28d031384d96107edc2c0d116e5
David Essien <davidessienshare@gmail.com>
2018-05-11 04:30:12 +0100

    add function to display flash messages

---
+++ app/helpers/session_helper.php
@@ -0,0 +1,26 @@
+<?php 
+    session_start();
+
+    //Flash message
+    //Example - flash('register_success'. 'You are now registered', 'alert alert-danger');
+    //Display in view - echo flasl('register_success')
+    function flash($name='', $message ='', $class='alert alert-success'){
+        if(!empty($name)){
+            if(!empty($message) && empty($_SESSION[$name])){
+                if(!empty($_SESSION[$name])){
+                    unset($_SESSION[$name]);
+                }
+                if(!empty($_SESSION[$name.'_class'])){
+                    unset($_SESSION[$name.'_class']);
+                }
+                $_SESSION[$name] = $name;
+                $_SESSION[$name.'_class'] = $class;
+            }else if(!empty($message) && !empty($_SESSION[$name])){
+                $class = !empty($_SESSION[$name.'_class']) ? $_SESSION[$name.'_class'] : '';
+                echo '<div class="'.$class.'" id="msg-flash">'.$_SESSION[$name].'</div>';
+                unset($_SESSION[$name]);
+                unset($_SESSION[$name.'_class']);
+            }
+        }
+    }
+?>
