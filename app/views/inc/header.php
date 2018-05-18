5a080e25b817e487ea20b58c9faa49c13fc86d6e
David Essien <davidessienshare@gmail.com>
2018-05-18 05:39:13 +0100

    include link to fontawesome

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,11 +5,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>public/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>public/fontawesome/web-fonts-with-css/css/fontawesome.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/css/styles.css">
 	<title><?php echo SITENAME ?></title>
 </head>
 <body>
 	<?php require_once 'navbar.php' ?>
 	<div class="container">
-
-	
