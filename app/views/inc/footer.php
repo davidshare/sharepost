c0fea3b0e97629dd361e07aec61d344f7c039554
David Essien <davidessienshare@gmail.com>
2018-05-18 08:11:59 +0100

    fix broken link to popper.js

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,6 +1,6 @@
 		
 		<script src="<?php echo URLROOT; ?>public/bootstrap/assets/js/vendor/jquery-slim.min.js"></script>
-		<script src="<?php echo URLROOT; ?>public/bootstrap/dist/css/popper.min.js"></script>
+		<script src="<?php echo URLROOT; ?>public/bootstrap/assets/js/vendor/popper.min.js"></script>
 		<script src="<?php echo URLROOT; ?>public/bootstrap/dist/js/bootstrap.min.js"></script>
 		<script src="<?php echo URLROOT; ?>/js/main.js"></script>
 		</div>
