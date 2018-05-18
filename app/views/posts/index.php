3294973d1045710083715573ca9c11439b400dde
David Essien <davidessienshare@gmail.com>
2018-05-18 05:50:19 +0100

    add basic structure or post index page

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -1,7 +1,16 @@
 <?php
 	require APPROOT.'/views/inc/header.php';
 ?>
-
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Posts</h2>
+        </div>
+        <div class="col-md-6">
+            <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-primary ml-auto">
+                <i class="fas fa-pencil">Add Post</i>
+            </a>
+        </div>
+    </div>
 <?php
 	require APPROOT.'/views/inc/footer.php';
 ?>
