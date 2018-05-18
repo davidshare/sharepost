e8fbeb3cfbc1d0020d11982db504e1355702a8cc
David Essien <davidessienshare@gmail.com>
2018-05-18 18:01:59 +0100

    display body of post, author and date created

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -13,14 +13,17 @@
     </div>
     <?php foreach($data['posts'] as $post) : ?>
         <div class="card card-body mb-3">
-            <h4 class="card-title">
-                <?php echo $post->title; ?>
-            </h4>
-            <p>
-                <?php echo $post->title; ?>
-            </p>
-        </div>
+            <h4 class="card-title"><?php echo $post->title; ?></h4>
+            <div class="bg-light p-2 mb-3">
+                Written by <?php echo $post->name.' on '.$post->postCreated; ?>
+            </div>
+            
+                <p class="card-text"><?php echo $post->body; ?></p>
+                <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
+            </div>
+        
     <?php endforeach ?>
+    </div>
 <?php
 	require APPROOT.'/views/inc/footer.php';
 ?>
