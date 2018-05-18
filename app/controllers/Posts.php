6ecf72fab9c7555a13ada0ac21980c8de0184518
David Essien <davidessienshare@gmail.com>
2018-05-18 22:24:25 +0100

    get data from post form and process it

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -18,10 +18,45 @@
         }
 
         public function add(){
-            $data = [
-                'title' => '',
-                'body' => ''
-            ];
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                //sanitize POST array
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+                $data = [
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => ''
+                ];
+                
+                //validate data
+                if(empty($data['title'])){
+                    $data['title_err'] = 'Please enter title';
+                }
+           
+                if(empty($data['body'])){
+                    $data['body_err'] = 'Please enter post content';
+                }
+
+                //Make sure no errors&
+                if(empty($data['title_err']) && empty($data['body_err'])){
+                    //Validated
+                    if($this->postModel->addPost($data)){
+                        flash('post_added', 'Post Added');
+                        redirect('posts');
+                    }else{
+                        die('Something went wrong');
+                    }
+                }else{
+                    $this->view('posts/add', $data);
+                }
+
+            }else{
+                $data = [
+                    'title' => '',
+                    'body' => ''
+                ];
+            }
 
             $this->view('posts/add', $data);
         }
