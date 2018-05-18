dd1cc53c6d0b330247b375298f9b2844b7a31791
David Essien <davidessienshare@gmail.com>
2018-05-18 12:07:06 +0100

    edit sql query to join users table

--- app/models/Post.php
+++ app/models/Post.php
@@ -7,7 +7,14 @@
         }
 
         public function getPosts(){
-            $this->db->query('SELECT * FROM posts');
+            $this->db->query('SELECT *,
+                            posts.id as postId,
+                            users.id as userId
+                            FROM posts
+                            INNER JOIN users
+                            ON posts.user_id = users.idate
+                            ORDER BY posts.created_at DESC
+                            ');
             $result = $this->db->resultSet();
             return $result;
         }
