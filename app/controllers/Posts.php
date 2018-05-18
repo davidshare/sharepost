73902c512abd1088ed7d30d0e805b15fe0b97667
David Essien <davidessienshare@gmail.com>
2018-05-18 05:16:56 +0100

    create variable to hold data for the post controller

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,7 +1,10 @@
 <?php
     class Posts extends Controller{
+        
+        
         public function index(){
-            $this->view('post/index');
+            $data = [];
+            $this->view('post/index', $data);
         }
     }
 ?>
