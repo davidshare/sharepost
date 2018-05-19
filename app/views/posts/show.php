9d9911dc3425a4ee272f3269fc00447b672bd699
David Essien <davidessienshare@gmail.com>
2018-05-19 02:32:38 +0100

    add back button to the show view

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -1,3 +1,7 @@
 <?php require APPROOT.'/views/inc/header.php'; ?>
+<a href="<?php echo URLROOT; ?>/posts" class="btn btn-light">
+	<i class="fas fa-backward"></i>
+	Back
+	</a>
 
 <?php require APPROOT.'/views/inc/footer.php'; ?>
