c740572878040a7e399c159d2446ed85f5251e27
David Essien <davidessienshare@gmail.com>
2018-05-19 02:25:32 +0100

    add method to save post to the database

--- app/models/Post.php
+++ app/models/Post.php
@@ -21,5 +21,21 @@
             return $result;
         }
 
+        public function addPost($data){
+            $this->db->query('INSERT INTO posts (title, user_id, body) VALUES(:title, :user_id, :body)');
+
+            //bind values
+            $this->db->bind(':title', $data['title']);
+            $this->db->bind(':user_id', $data['user_id']);
+            $this->db->bind(':body', $data['body']);
+
+            //Execute
+            if($this->db->execute()){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
     }
 ?>
