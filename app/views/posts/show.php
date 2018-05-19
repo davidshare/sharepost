550c1f1c1910e12889874d7c4cf617d9d4cfa0cf
David Essien <davidessienshare@gmail.com>
2018-05-19 03:19:56 +0100

    get the user and post data and display it

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -3,5 +3,13 @@
 	<i class="fas fa-backward"></i>
 	Back
 	</a>
+    <br>
+    <h1><?php echo $data['post']->title; ?></h1>
+    <div class="bg-secondary text-white p-2 mb-3">
+        Written by <?php echo $data['user']->name.' on '.$data['post']->created_at; ?>
+    </div>
+    <p><?php echo $data['post']->body; ?> </p>
+    
+    
 
 <?php require APPROOT.'/views/inc/footer.php'; ?>
